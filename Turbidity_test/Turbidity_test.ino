ca5f59cb0e886e8e3d524436375924a35a410db0
RoopsHub <roopit28@gmail.com>
2025-10-02 16:23:00 +0300

    Trubidity sample

---
+++ Turbidity_test/Turbidity_test.ino
@@ -0,0 +1,18 @@
+// Turbidity quick tester
+const int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("Turbidity tester starting...");
+}
+
+void loop() {
+  int raw = analogRead(sensorPin);     // 0 - 1023
+  float voltage = raw * (5.0 / 1023.0);
+  Serial.print("ADC: ");
+  Serial.print(raw);
+  Serial.print("   Voltage: ");
+  Serial.println(voltage, 3);
+  delay(500);
+}
